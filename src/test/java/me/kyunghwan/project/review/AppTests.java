5ae2a39c8028b279b01669242fe4436db6c621ed
ber01 <ber01@naver.com>
2020-10-26 23:23:09 +0900

    Project init

---
+++ src/test/java/me/kyunghwan/project/review/AppTests.java
@@ -0,0 +1,13 @@
+package me.kyunghwan.project.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
