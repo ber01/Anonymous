371fa74d51d7a6709a927599e2b0806a69628159
ber01 <ber01@naver.com>
2020-11-03 00:38:48 +0900

    [#9] MovieGenre 엔티티 추가

---
+++ src/main/java/me/kyunghwan/review/moviegenre/MovieGenreRepository.java
@@ -0,0 +1,9 @@
+package me.kyunghwan.review.moviegenre;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieGenreRepository extends JpaRepository<MovieGenre, Long> {
+
+
+
+}
